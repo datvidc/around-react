49170eca0bf6b024a9237745be28fea52e224b69
David Christoffersen <d@vidc.dk>
2020-08-15 15:08:30 -0400

    header component

---
+++ project10/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Vector from '../images/Vector.svg';
+
+function Header {
+  return (
+  <header className="header">
+    <img className="header__logo" src={Vector} alt="Around the US" />
+  </header>
+  );
+}

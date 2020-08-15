3346fb1db355f14c82878c2660def384dfae2aea
David Christoffersen <d@vidc.dk>
2020-08-15 15:05:35 -0400

    Cleaning up app.js

--- project10/src/components/App.js
+++ project10/src/components/App.js
@@ -3,125 +3,119 @@ import Vector from '../images/Vector.svg';
 
 function App() {
   return (
- <div >
+<div >
 
-<header className="header">
-  <img className="header__logo" src={Vector} alt="Around the US" />
-</header>
+  <header className="header">
+    <img className="header__logo" src={Vector} alt="Around the US" />
+  </header>
 
-<main>
-  <section className="profile">
-    <div className="profile__avatar-overlay">
-      <img src="./images/image.png" alt="J. Cousteau- with his iconic red hat" className="profile__avatar" />
-      <div className="profile__avatar-hover" />
-    </div>
-    <div className="profile__text">
-      <div className="profile__namebox">
-        <p className="profile__name">Jacques Cousteau</p>
-        <div className="profile__edit" />
+  <main>
+    <section className="profile">
+      <div className="profile__avatar-overlay">
+        <img src="./images/image.png" alt="J. Cousteau- with his iconic red hat" className="profile__avatar" />
+        <div className="profile__avatar-hover" />
       </div>
-      <p className="profile__title">Explorer</p>
+      <div className="profile__text">
+        <div className="profile__namebox">
+          <p className="profile__name">Jacques Cousteau</p>
+          <div className="profile__edit" />
+        </div>
+        <p className="profile__title">Explorer</p>
+      </div>
+      <button className="profile__add"> </button>
+    </section>
+    <section className="elements">
+      <ul className="elements__list">
+      </ul>
+    </section>
+  </main>
+
+  <footer className="footer ">
+    <p className="footer__copyright ">© 2020 Around The U.S.</p>
+  </footer>
+
+  <div className="popup popup__edit-picture">
+    <div className="popup__container popup__container_userImg">
+      <button type="submit " className="popup__close"> </button>
+      <form className="popup__edit-form popup__edit-userImg">
+        <h3 className="popup__userImg-heading">Change profile picture </h3>
+        <label className="popup__label-group">
+          <input id="user-link" type="url" placeholder="Image link" className="popup__edit_invalid popup__edit popup__url popup_detail " name="imgUrl" required />
+          <span id="user-link--error" className="popup__error-input popup__url_error">Please enter a URL.</span>
+        </label>
+        <button type="submit" className="popup__save popup__userImg"> Yes </button>
+      </form>
     </div>
-    <button className="profile__add"> </button>
-  </section>
-  <section className="elements">
-    <ul className="elements__list">
-    </ul>
-  </section>
-</main>
-
-<footer className="footer ">
-  <p className="footer__copyright ">© 2020 Around The U.S.</p>
-</footer>
-
-<div className="popup popup__edit-picture">
-  <div className="popup__container popup__container_userImg">
-    <button type="submit " className="popup__close"> </button>
-    <form className="popup__edit-form popup__edit-userImg">
-      <h3 className="popup__userImg-heading">Change profile picture </h3>
-      <label className="popup__label-group">
-        <input id="user-link" type="url" placeholder="Image link" className="popup__edit_invalid popup__edit popup__url popup_detail " name="imgUrl" required />
-        <span id="user-link--error" className="popup__error-input popup__url_error">Please enter a URL.</span>
-      </label>
-      <button type="submit" className="popup__save popup__userImg"> Yes </button>
-    </form>
   </div>
-</div>
 
-<div className="popup popup__delete-confirm ">
-  <div className="popup__container popup__container_delete ">
-    <button type="submit " className="popup__close "> </button>
-    <form className="popup__edit-form popup__edit-delete ">
-      <h3 className="popup__delete-heading ">Are you sure ? </h3>
-      <button type="submit " className="popup__save popup__delete "> Yes </button>
-    </form>
+  <div className="popup popup__delete-confirm ">
+    <div className="popup__container popup__container_delete ">
+      <button type="submit " className="popup__close "> </button>
+      <form className="popup__edit-form popup__edit-delete ">
+        <h3 className="popup__delete-heading ">Are you sure ? </h3>
+        <button type="submit " className="popup__save popup__delete "> Yes </button>
+      </form>
+    </div>
   </div>
-</div>
 
-<div className=" popup popup__changetext ">
-  <div className="popup__container ">
-    <button type="submit " className="popup__close "> </button>
-    <form className="popup__edit-form">
-      <h3 className="popup__heading">Edit profile</h3>
-      <label className="popup__label-group">
-        <input id="form-name" type="text" minLength={2} maxLength={40} required pattern="[A-Za-z -]{2,40}" placeholder="Name" className="popup__edit popup__name popup_head" name="ProfileName" />
-        <span id="form-name--error" className="popup__edit_error popup__name-error" />
-      </label>
-      <label className="popup__label-group">
-        <input id="form-status" type="text" minLength={2} maxLength={200} required placeholder="About" className="popup__edit popup__title popup_detail" name="profileTitle" />
-        <span id="form-status--error" className="popup__edit_error popup__title-error" />
-      </label>
-      <button type="submit " className="popup__save "> Save</button>
-    </form>
+  <div className=" popup popup__changetext ">
+    <div className="popup__container ">
+      <button type="submit " className="popup__close "> </button>
+      <form className="popup__edit-form">
+        <h3 className="popup__heading">Edit profile</h3>
+        <label className="popup__label-group">
+          <input id="form-name" type="text" minLength={2} maxLength={40} required pattern="[A-Za-z -]{2,40}" placeholder="Name" className="popup__edit popup__name popup_head" name="ProfileName" />
+          <span id="form-name--error" className="popup__edit_error popup__name-error" />
+        </label>
+        <label className="popup__label-group">
+          <input id="form-status" type="text" minLength={2} maxLength={200} required placeholder="About" className="popup__edit popup__title popup_detail" name="profileTitle" />
+          <span id="form-status--error" className="popup__edit_error popup__title-error" />
+        </label>
+        <button type="submit " className="popup__save "> Save</button>
+      </form>
+    </div>
   </div>
-</div>
 
 
-<div className="popup popup__addcard">
-  <div className="popup__container popup__container_addcard">
-    <button type="submit" className="popup__close popup__close_addcard"> </button>
-    <h3 className="popup__heading">New place</h3>
-    <form className="popup__edit-form popup__edit-form_add">
-      <label className="popup__label-group ">
-        <input id="form-title" type="text" minLength={2} maxLength={40} placeholder="Title" className="popup__edit_invalid popup__edit popup__place popup_head" name="placename" required />
-        <span id="form-title--error" className="popup__error-input popup__place-error"> Please fill out this field.</span>
-      </label>
-      <label className="popup__label-group">
-        <input id="form-link" type="url" placeholder="Image link" className="popup__edit_invalid popup__edit popup__url popup_detail" name="popupurl" required />
-        <span id="form-link--error" className="popup__error-input popup__url_error">Please enter a web address.</span>
-      </label>
-      <button type="submit" className="popup__save_invalid popup__save popup__save_addcard"> Create</button>
-    </form>
+  <div className="popup popup__addcard">
+    <div className="popup__container popup__container_addcard">
+      <button type="submit" className="popup__close popup__close_addcard"> </button>
+      <h3 className="popup__heading">New place</h3>
+      <form className="popup__edit-form popup__edit-form_add">
+        <label className="popup__label-group ">
+          <input id="form-title" type="text" minLength={2} maxLength={40} placeholder="Title" className="popup__edit_invalid popup__edit popup__place popup_head" name="placename" required />
+          <span id="form-title--error" className="popup__error-input popup__place-error"> Please fill out this field.</span>
+        </label>
+        <label className="popup__label-group">
+          <input id="form-link" type="url" placeholder="Image link" className="popup__edit_invalid popup__edit popup__url popup_detail" name="popupurl" required />
+          <span id="form-link--error" className="popup__error-input popup__url_error">Please enter a web address.</span>
+        </label>
+        <button type="submit" className="popup__save_invalid popup__save popup__save_addcard"> Create</button>
+      </form>
+    </div>
   </div>
-</div>
 
-<div className="popup popup__img">
-  <div className="popup__card popup_image">
-    <button type="submit " className="popup__close popup__closeimg"> </button>
-    <img src="# " alt=" " className="popup__image popImg" />
-    <p className="popup__imgtext">PlaceName </p>
+  <div className="popup popup__img">
+    <div className="popup__card popup_image">
+      <button type="submit " className="popup__close popup__closeimg"> </button>
+      <img src="# " alt=" " className="popup__image popImg" />
+      <p className="popup__imgtext">PlaceName </p>
+    </div>
   </div>
-</div>
 
 
   <template className="element__elem ">
-<li className="elements__element ">
-  <button id="elements__trash " className="elements__trash " />
-  <img className="elements__image " src="# " alt=" " />
-  <div className="elements__photo-bottom ">
-    <p className="elements__text "> </p>
-    <button className="elements__heart "></button>
-    <p className="elements__heart_likes "> 0</p>
-  </div>
-</li>
-
+  <li className="elements__element ">
+    <button id="elements__trash " className="elements__trash " />
+    <img className="elements__image " src="# " alt=" " />
+    <div className="elements__photo-bottom ">
+      <p className="elements__text "> </p>
+      <button className="elements__heart "></button>
+      <p className="elements__heart_likes "> 0</p>
+    </div>
+  </li>
   </template>
-
-
-
-
-  </div>
-
+</div>
   );
 }
 

835406593d30f8eaa89e946036d2b3b532971198
David Christoffersen <d@vidc.dk>
2020-08-22 19:06:29 -0400

    Making profile states

--- project10/src/components/Main.js
+++ project10/src/components/Main.js
@@ -5,23 +5,22 @@ import defaultAvatarPicture from "../images/DC-img.png";
 
 function Main(props) {
 
-/*   const [userName, setUserName] = useState("Lacking Gravitas");
+  const [userName, setUserName] = useState("Lacking Gravitas");
   const [userDescription, setUserDescription] = useState("Space Ship");
   const [userAvatar, setUserAvatar] = useState(defaultAvatarPicture);
 
- */
 
 
   return (
     <main>
     <section className="profile">
       <div className="profile__avatar-overlay" onClick={props.onAvatarClick}>
-        <img src={image} alt="J. Cousteau- with his iconic red hat" className="profile__avatar" />
+        <img src={userAvatar} alt="image of ${userName}" className="profile__avatar" />
         <div className="profile__avatar-hover" />
       </div>
       <div className="profile__text">
         <div className="profile__namebox">
-          <p className="profile__name">Jacques Cousteau</p>
+          <p className="profile__name">{userName}</p>
           <button className="profile__edit" onClick={props.onEditProfile} />
         </div>
         <p className="profile__title">Explorer</p>

7b1d2f0f761d456f433f09b5266d876e6a058243
David Christoffersen <d@vidc.dk>
2020-08-23 15:13:18 -0400

    main js work

--- project10/src/components/Main.js
+++ project10/src/components/Main.js
@@ -55,6 +55,22 @@ function Main(props) {
     </section>
     <section className="elements">
       <ul className="elements__list">
+        {cards ? (cards.map((card, index) => {
+          return (
+      <template className="element__elem">
+        <li className="elements__element">
+          <button id="elements__trash " className="elements__trash " />
+          <img className="elements__image " src={card} alt=" " />
+          <div className="elements__photo-bottom ">
+            <p className="elements__text "> </p>
+            <button className="elements__heart "></button>
+            <p className="elements__heart_likes "> 0</p>
+          </div>
+        </li>
+      </template> )
+
+
+        })) : ""}
       </ul>
     </section>
   </main>

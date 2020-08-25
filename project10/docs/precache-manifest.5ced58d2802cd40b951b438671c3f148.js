9ed9d2c958a099a4468a967e34157117826b52f5
David Christoffersen <d@vidc.dk>
2020-08-25 10:51:44 -0400

    rename docs folder

---
+++ project10/docs/precache-manifest.5ced58d2802cd40b951b438671c3f148.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88310abe0569885886d94c2a990584b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8022b68dee0f1ded23ad",
+    "url": "/static/css/main.8ec76294.chunk.css"
+  },
+  {
+    "revision": "9ad4fd0c80012565d03d",
+    "url": "/static/js/2.c3e969b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3e969b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8022b68dee0f1ded23ad",
+    "url": "/static/js/main.a738c7a8.chunk.js"
+  },
+  {
+    "revision": "889798513dec9b7f4c5f",
+    "url": "/static/js/runtime-main.b3d80e58.js"
+  },
+  {
+    "revision": "a8f51591dac779307d8b866503e55634",
+    "url": "/static/media/DC-img.a8f51591.png"
+  },
+  {
+    "revision": "9c261a6658541ffb9dc4dfa3be89b01f",
+    "url": "/static/media/Edit.9c261a66.svg"
+  },
+  {
+    "revision": "14a176339fc00af3ae93be979f1593cc",
+    "url": "/static/media/Inter-Black.14a17633.woff2"
+  },
+  {
+    "revision": "b90562e0cf2dcfedca0b93ad7fa447d8",
+    "url": "/static/media/Inter-Black.b90562e0.woff"
+  },
+  {
+    "revision": "41751fc730332b9facaea93b826197c1",
+    "url": "/static/media/Inter-BlackItalic.41751fc7.woff"
+  },
+  {
+    "revision": "d92bb7894f88ca8ebf47e041ea8328e4",
+    "url": "/static/media/Inter-BlackItalic.d92bb789.woff2"
+  },
+  {
+    "revision": "001893789f7f342b520f29ac8af7d6ca",
+    "url": "/static/media/Inter-Bold.00189378.woff"
+  },
+  {
+    "revision": "e7ae98681edfa1df7f1e3ebba0d4fb88",
+    "url": "/static/media/Inter-Bold.e7ae9868.woff2"
+  },
+  {
+    "revision": "a0e89d60e007ea22dad528c5dec09cd4",
+    "url": "/static/media/Inter-ExtraBold.a0e89d60.woff2"
+  },
+  {
+    "revision": "b82eef497995d28fffc2ef185211e282",
+    "url": "/static/media/Inter-ExtraBold.b82eef49.woff"
+  },
+  {
+    "revision": "0713a6d4f4b4357758518720b363f322",
+    "url": "/static/media/Inter-ExtraLight.0713a6d4.woff"
+  },
+  {
+    "revision": "8381bcfb1339ad96a5675d5dcfcbcd09",
+    "url": "/static/media/Inter-ExtraLight.8381bcfb.woff2"
+  },
+  {
+    "revision": "a1f9e860d918b33aa82a0c2c10d30d6f",
+    "url": "/static/media/Inter-Light.a1f9e860.woff2"
+  },
+  {
+    "revision": "c58ea231468353018b9f8ca321f18282",
+    "url": "/static/media/Inter-Light.c58ea231.woff"
+  },
+  {
+    "revision": "95b8a98959d1af9ab432d7ffe295ef94",
+    "url": "/static/media/Inter-Medium.95b8a989.woff"
+  },
+  {
+    "revision": "c709803c3cab6f1116039e881ecf531a",
+    "url": "/static/media/Inter-Medium.c709803c.woff2"
+  },
+  {
+    "revision": "1e081edc16d92d42aeccec760174fbf4",
+    "url": "/static/media/Inter-Regular.1e081edc.woff2"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/static/media/Inter-Regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "19b57197b819695d334b9961ee41910e",
+    "url": "/static/media/Inter-SemiBold.19b57197.woff"
+  },
+  {
+    "revision": "4663322354d4300146ac57cd55daabf2",
+    "url": "/static/media/Inter-SemiBold.46633223.woff2"
+  },
+  {
+    "revision": "035ae3d872fc8e111756711edc400bda",
+    "url": "/static/media/Inter-Thin.035ae3d8.woff"
+  },
+  {
+    "revision": "9c96c7a2494ef60e8c2c75ad9baf1c5c",
+    "url": "/static/media/Inter-Thin.9c96c7a2.woff2"
+  },
+  {
+    "revision": "782b5eb4021f2fa988f87d7a6df73a68",
+    "url": "/static/media/Vector.782b5eb4.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/static/media/editavatar.d476a8b8.svg"
+  },
+  {
+    "revision": "a8acc871456b1001597672e02c5199ca",
+    "url": "/static/media/heart.a8acc871.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/static/media/heart_clicked.5bbb05f7.svg"
+  },
+  {
+    "revision": "0d10d5301916476f3e5ca3e9afa58780",
+    "url": "/static/media/image.0d10d530.png"
+  },
+  {
+    "revision": "e0270e6710268c67a26289e12877ccbb",
+    "url": "/static/media/plus.e0270e67.svg"
+  },
+  {
+    "revision": "4220563aa7e7f6bb1d562a95174678b6",
+    "url": "/static/media/trashcan.4220563a.svg"
+  }
+]);

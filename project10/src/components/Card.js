74375189b15dbca06bdc21de8943272a4300adc3
David Christoffersen <d@vidc.dk>
2020-09-08 12:31:31 -0400

    Done with 1

--- project10/src/components/Card.js
+++ project10/src/components/Card.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Card(props) {
-
+  const currentUser = React.useContext(CurrentUserContext);
+  const isOwn = props.card.owner._id === currentUser._id;
+  const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+  
   function handleClick() {
   props.onImgClick(props.card);
 }
@@ -9,11 +13,11 @@ function Card(props) {
 return(
 
     <li key={props.index} className="elements__element">
-      <button id="elements__trash " className="elements__trash " />
+     { isOwn ? <button id="elements__trash " className="elements__trash " /> : ""} 
       <img className="elements__image" onClick={handleClick} src={props.card.link} alt={props.card.name} />
       <div className="elements__photo-bottom ">
         <p className="elements__text "> {props.card.name} </p>
-        <button className="elements__heart "></button>
+      {isLiked ? <button  className="elements__heart elements__heart_clicked"></button> : <button className="elements__heart"></button> }
         <p className="elements__heart_likes "> {props.card.likes.length}</p>
       </div>
     </li>

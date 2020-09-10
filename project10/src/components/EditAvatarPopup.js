31e32d3c20631ad37f6c8175ba0847626e4f0d86
David Christoffersen <d@vidc.dk>
2020-09-09 20:50:39 -0400

    Now looking for props

--- project10/src/components/EditAvatarPopup.js
+++ project10/src/components/EditAvatarPopup.js
@@ -6,18 +6,29 @@ function EditAvatarPopup(props) {
 
      // use the context after import. CurrentUserContext
      const currentUser = React.useContext(CurrentUserContext);
+     React.useEffect(() => {
+        setavatarUrl(currentUser.avatar);
+      }, [currentUser]);
+    const [avatarUrl, setavatarUrl] = React.useState(currentUser.avatar);
 
     //Introducing Ref of avatarUrl...
     const avatarValue = React.useRef();
 
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.onUpdateAvatar(avatarUrl);
+
+    }
+
     return (
 
-        <PopupWithForm name="edit-picture" isOpen={props.isOpen} heading="Change profile picture" buttonText="Yes" closeItAll={props.onClose} >
+        <PopupWithForm name="edit-picture" isOpen={props.isOpen} heading="Change profile picture" buttonText="Yes" closeItAll={props.onClose} onSubmit={handleSubmit} >
 				<label className="popup__label-group">
 				  <input ref={avatarValue} id="user-link" type="url" placeholder={currentUser.avatar} className="popup__edit_invalid popup__edit popup__url popup_detail " name="imgUrl" required />
 					<span id="user-link--error" className="popup__error-input popup__url_error">Please enter a URL.</span>
 				</label>
-			</PopupWithForm>
+		</PopupWithForm>
     )
 
 
